a5eeb4ad9ece6f9abd22cdb516448963f4a2edec
Kandito Agung Wicaksono <i-kandito@isi.co.id>
2015-03-20 01:38:36 +0700

    update spider kompas

--- crawler/spiders/KompasSpider.py
+++ crawler/spiders/KompasSpider.py
@@ -52,7 +52,7 @@ class KompasSpider(CrawlSpider):
     rules = (
         # Extract links matching 'read' and parse them with the spider's method parse_item
         # Rule(SgmlLinkExtractor(allow=('', )), follow=True),
-        Rule(SgmlLinkExtractor(allow=('index','p=',), deny=('reg','sso','login','utm_source=wp')),follow=True),
+        Rule(SgmlLinkExtractor(allow=('indeks','p=',), deny=('reg','sso','login','utm_source=wp')),follow=True),
         Rule(SgmlLinkExtractor(allow=('read/', 'read/xml/',), deny=('reg','sso','login','utm_source=wp')),follow=True, callback='parse_item'),
     )
 
@@ -74,11 +74,21 @@ class KompasSpider(CrawlSpider):
 
             """Getting Title"""
             if 'bola.kompas.com' in response.url:
-                title = response.xpath('//html/body/div/div[6]/div/div[1]/div[1]/div[2]/h2').extract()[0]
+                title = response.xpath('//html/body/div/div[6]/div/div[1]/div[1]/div[2]/h2/text()').extract()[0]
+            elif 'tekno.kompas.com' in response.url:
+                title = response.xpath('//html/body/div[1]/div[2]/div[5]/div[1]/div[2]/div/div[2]/div[1]/h1').extract()[0]
+            elif 'entertainment.kompas.com':
+                title = response.xpath('//html/body/div[2]/div/div/div[7]/div[4]/span[2]/text()').extract()[0]
+            elif 'otomotif.kompas.com' in response.url:
+                title = response.xpath('//*[@id='skrol']/div[1]/div[4]/h1/text()').extract()[0]
             elif 'health.kompas.com' in response.url:
-                title = response.xpath('//html/body/div[3]/div[4]/div[4]/h1').extract()[0]
-            else:
-                title = response.xpath('//h2/text()').extract()[0]
+                title = response.xpath('//html/body/div[3]/div[4]/div[4]/h1/text()').extract()[0]     
+            elif 'female.kompas.com' in response.url:
+                title = response.xpath('//*[@id='wrap-skin']/div/div[2]/div/div[1]/div[8]/div[3]/div[2]').extract()[0]
+            elif 'properti.kompas.com' in response.url:
+                title = response.xpath('//html/body/div[1]/div[5]/div[3]/div[1]/div[2]/text()').extract()[0]
+            else: 
+                title = response.xpath('//h2/text()')
 
             if len(title):
                 news['title'] = helper.html_to_string(title)
@@ -86,29 +96,99 @@ class KompasSpider(CrawlSpider):
                 raise DropItem("No title")
 
             """Getting Content"""
-            content = response.xpath('//span[@class="kcmread1114"]').extract()
+            if 'tekno.kompas.com' in response.url:
+                content = response.xpath('//html/body/div[1]/div[2]/div[5]/div[1]/div[2]/div/div[2]/div[2]/div[4]/text()').extract()[0]
+            elif 'entertainment.kompas.com' in response.url:
+                content = response.xpath('//html/body/div[2]/div/div/div[7]/div[4]/div[9]/div[5]/text()').extract()[0]
+            elif 'otomotif.kompas.com' in response.url:
+                content = response.xpath('//*[@id='skrol']/div[1]/div[4]/div[4]/div[1]/p/text()').extract()[0]
+            elif 'health.kompas.com' in response.url:
+                content = response.xpath('//html/body/div[3]/div[4]/div[4]/div[6]/div[5]/text()').extract()[0]
+            elif 'female.kompas.com' in response.url:
+                content = response.css('#article_body').extract()[0]
+            elif 'properti.kompas.response' in response.url:
+                content = response.xpath('//html/body/div[1]/div[5]/div[3]/div[1]/div[8]/div[5]/text()').extract()[0]
+            elif 'travel.kompas.com' in response.url:
+                content = response.css('.nml').extract()[0]
+            else:
+                content = response.xpath('//span[@class="kcmread1114"]').extract()[0]
+            
             if len(content) > 0:
-                news['content'] = helper.html_to_string(content[0])
+                news['content'] = helper.html_to_string(content)
             else:
                 raise DropItem("No content")
 
             """Getting Publish date"""""
             publish = response.xpath('//div[@class="grey small mb2"]/text()').extract()
+            if 'tekno.kompas.com' in response.url:
+                publish = response.xpath('html/body/div[1]/div[2]/div[5]/div[1]/div[2]/div/div[2]/div[2]/div[1]/div[1]/a[2]/text()').extract()[0]
+            elif 'entertainment.kompas.com' in response.url:
+                publish = response.xpath('//html/body/div[2]/div/div/div[7]/div[4]/div[5]/div[1]/span[2]/text()').extract()[0]
+            elif 'otomotif.kompas.com' in response.url:
+                publish = response.xpath('//*[@id='skrol']/div[1]/div[4]/div[4]/div[1]/p/text()').extract()[0]
+            elif 'health.kompas.com' in response.url:
+                publish = response.xpath('//html/body/div[3]/div[4]/div[4]/div[2]/div[1]/span/text()').extract()[0]
+            elif 'female.kompas.com' in response.url:
+                publish = response.xpath('//*[@id='wrap-skin']/div/div[2]/div/div[1]/div[8]/div[3]/div[4]/div[1]/span/text()').extract()[0]
+            elif 'properti.kompas.response' in response.url:
+                publish = response.xpath('//html/body/div[1]/div[5]/div[3]/div[1]/div[4]/div[1]/span[2]/text()').extract()[0]
+            else:
+                publish = response.xpath('//div[@class="grey small mb2"]/text()').extract()[0]
+
             if len(publish) > 0:
                 news['publish'] = helper.kompas_date(publish[0]) - timedelta(hours=7)
             else:
                 news['publish'] = "";
 
             """Getting Author"""
-            author = response.xpath('//html/body/div[1]/div[6]/div/div[1]/div[3]/div[3]/div[2]/div[2]/table/tbody/tr[1]/td[2]').extract()
+            if 'tekno.kompas.com' in response.url:
+                author = response.xpath('//html/body/div[1]/div[2]/div[5]/div[1]/div[2]/div/div[2]/div[2]/div[1]/div[1]/a[1]/text()').extract()[0]
+            elif 'entertainment.kompas.com' in response.url:
+                author = response.xpath('//html/body/div[2]/div/div/div[7]/div[4]/div[11]/div[3]/text()').extract()[0]
+            elif 'otomotif.kompas.com' in response.url:
+                author = response.xpath('//*[@id='skrol']/div[1]/div[4]/div[4]/div[2]/table/tbody/tr[1]/td[2]/text()').extract()[0]
+            elif 'health.kompas.com' in response.url:
+                author = response.xpath('//html/body/div[3]/div[4]/div[4]/div[2]/div[1]/span[1]/text()').extract()[0]
+            elif 'female.kompas.com' in response.url:
+                author = response.xpath('//*[@id='article_body']/div[5]/text()').extract()[0]
+            elif 'properti.kompas.response' in response.url:
+                author = response.xpath('//html/body/div[1]/div[5]/div[3]/div[1]/div[4]/div[1]/span[1]/text()').extract()[0]
+            elif 'travel.kompas.com' in response.url:
+                author = response.xpath('//html/body/div[2]/div[5]/div/div[1]/div[2]/div[1]/h2/text()').extract()[0]
+            elif 'internasional.kompas.com' in response.url:
+                author = response.xpath('//html/body/div[1]/div[6]/div/div[1]/div[4]/div[3]/div[2]/div[2]/table/tbody/tr[1]/td[2]/text()').extract()[0]
+            elif 'bisniskeuangan.kompas.com' in response.url:
+                author = response.xpath('//html/body/div[2]/div[5]/div/div[1]/div[1]/div[3]/div[2]/div[2]/table/tbody/tr[1]/td[2]/text()').extract()[0]
+            elif 'bola.kompas.com' in response.url:
+                author = response.xpath('//html/body/div/div[6]/div/div[1]/div[1]/div[4]/div[2]/div[2]/table/tbody/tr[1]/td[2]').extract()[0]
+            else:
+                author = response.xpath('//html/body/div[1]/div[6]/div/div[1]/div[3]/div[3]/div[2]/div[2]/table/tbody/tr[1]/td[2]/text()').extract()[0]
+            
             if len(author) > 0:
                 news['author'] = helper.html_to_string(author[0])
             else:
-                news['author'] = " ";
+                news['author'] = "";
 
             """Getting location of news"""
-            location = news['content'][0:news['content'].find(',')]
-            news['location'] = location
+            if 'tekno.kompas.com' in response.url:
+                location = response.xpath('//html/body/div[1]/div[2]/div[5]/div[1]/div[2]/div/div[2]/div[2]/div[4]/strong/text()').extract()[0]
+            elif 'entertainment.kompas.com' in response.url:
+                location = response.xpath('//html/body/div[2]/div/div/div[7]/div[4]/div[9]/div[5]/strong/text()').extract()[0]
+            elif 'otomotif.kompas.com' in response.url:
+                location = response.xpath('//*[@id='skrol']/div[1]/div[4]/div[4]/div[1]/p/strong[1]/text()').extract()[0]
+            elif 'health.kompas.com' in response.url:
+                location = ""
+            elif 'female.kompas.com' in response.url:
+                location = ""
+            elif 'properti.kompas.response' in response.url:
+                location = response.xpath('//html/body/div[1]/div[5]/div[3]/div[1]/div[8]/div[5]/strong/text()').extract()[0]
+            else:
+                location = news['content'][0:news['content'].find(',')]
+            
+            if len(location) > 0:
+                news['location'] = location
+            else:
+                news['location'] = ""
 
             """Getting Timestamp and Provider"""
             news['timestamp']= datetime.utcnow()
