7b4521f19d37615afcda11de8ed4bfb6c77c91e7
zakazai <zaka.zaidan@ui.ac.id>
2015-03-24 06:36:53 +0700

    fix author bug in tempo

--- crawler/spiders/TempoSpider.py
+++ crawler/spiders/TempoSpider.py
@@ -23,8 +23,8 @@ If you want crawling and follow link , Change Kompas
 class TempoSpider(CrawlSpider):
     name = "tempo"
     allowed_domains = ["tempo.co","komunika.tempo.co",]
-    start_urls = ["tempo.co"]
-
+    start_urls = ["http://www.tempo.co/read/news/2015/01/27/078637910/Nurul-Arifin-Jokowi-Direcoki-Partai-Pendukung"]
+    #start_urls = ["http://www.tempo.co/read/news/2015/02/04/140639978/Tahun-2015-Wajib-Kunjungi-Filipina"]
     rules = (
         Rule(SgmlLinkExtractor(allow=('indeks'), deny=('reg','sso','login','utm_source=wp')),follow=True),
         Rule(SgmlLinkExtractor(allow=('read/'), deny=('reg','sso','login','utm_source=wp')),follow=True, callback='parse_item'),
@@ -54,17 +54,41 @@ class TempoSpider(CrawlSpider):
         else:
             title = helper.html_to_string(response.xpath("//*[@class='title']").extract()[0])
             content = helper.html_to_string(helper.item_merge(response.xpath("//*[@class='artikel']/p").extract()))
-            author = content.rsplit('.',1)[1]
+            details = response.xpath("//*[@class='artikel']/p/strong/text()").extract()
+            if "populer" in details[-1]:
+                location = details[1]
+                author = details[2]
+            else:
+                location = helper.html_to_string(response.xpath("//strong").extract()[-2])
+                author = content.rsplit('.',1)[1]
             content = content.split('-',1)[1].rsplit('.',1)[0]
-            publish =  helper.kompas_date(helper.html_to_string(response.xpath("//*[@class='submitted']").extract()[0]))
-            location = helper.html_to_string(response.xpath("//strong").extract()[-2])
+            publish =  self.tempo_date(helper.html_to_string(response.xpath("//*[@class='submitted']").extract()[0]))
             
         news['title'] = title
         news['content'] = content
         news['publish'] = publish
         news['author'] = author
-        news['location'] = location
+        news['location'] = location.replace('-','').strip()
         news['timestamp']= datetime.utcnow()
         news['provider'] = "tempo.co"
         
         yield news
+
+    def tempo_date(self, plain_string):
+        if plain_string == None:
+            return None
+
+        plain_string = helper.html_to_string(plain_string)
+
+        string_date = plain_string[plain_string.find(",") + 2 : plain_string.find("|") - 1]
+        string_time = plain_string[plain_string.find("|") + 2 : len(plain_string)]
+
+        date = string_date.split(" ")
+        day = date[0]
+        year = date[2]
+        ms = date[1]
+        month = helper.get_month(ms)
+        hour = string_time[0:2]
+        minute = string_time[3:5]
+
+        return helper.formatted_date(year,month,day,hour,minute)
