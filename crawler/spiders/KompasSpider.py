a61d74d13df87178441debaf4d81cd650d4bdebf
kandito <kandito.agung@ui.ac.id>
2015-03-27 22:46:25 +0700

    perbaikan master

--- crawler/spiders/KompasSpider.py
+++ crawler/spiders/KompasSpider.py
@@ -20,12 +20,8 @@ If you want crawling and follow link , Change Kompas
     Change start_urls with 'http://kompas.com'
     Change 'parse' method to 'parse_item'
 """
-<<<<<<< HEAD
 
 class KompasSpider(CrawlSpider):
-=======
-class KompasSpider(Spider):
->>>>>>> spider_kompas
     name = "kompas"
     allowed_domains = [
 	    "kompas.com",
@@ -46,29 +42,14 @@ class KompasSpider(Spider):
         "female.kompas.com",
         "properti.kompas.com",
         "travel.kompas.com",
-<<<<<<< HEAD
         "foto.kompas.com",
         "video.kompas.com",
         ]
 
     start_urls = ["http://kompas.com"]
-=======
-        ]
-    # start_urls = [  "http://kompas.com",
-	# 				#"http://indeks.kompas.com/?tanggal=1&bulan=3&tahun=2008&pos=indeks",
-	# 				#"http://indeks.kompas.com/?tanggal=1&bulan=4&tahun=2008&pos=indeks",
-	# 				#"http://indeks.kompas.com/?tanggal=1&bulan=5&tahun=2008&pos=indeks",
-	# 				#"http://indeks.kompas.com/?tanggal=1&bulan=6&tahun=2008&pos=indeks",
-	# 				#"http://indeks.kompas.com/?tanggal=1&bulan=7&tahun=2008&pos=indeks",
-	# 				#"http://indeks.kompas.com/?tanggal=1&bulan=8&tahun=2008&pos=indeks",
-	# 				#"http://indeks.kompas.com/?tanggal=1&bulan=9&tahun=2008&pos=indeks",
-	# 				#"http://indeks.kompas.com/?tanggal=1&bulan=10&tahun=2008&pos=indeks",
-	# 				#"http://indeks.kompas.com/?tanggal=1&bulan=11&tahun=2008&pos=indeks",
-	# 				#"http://indeks.kompas.com/?tanggal=1&bulan=12&tahun=2008&pos=indeks",
-	# ]
 
-    start_urls = ["http://nasional.kompas.com/read/2015/03/19/12142221/Tedjo.7.WN.Tiongkok.Terduga.ISIS.Bergabung.dengan.Kelompok.Poso"]
->>>>>>> spider_kompas
+
+    start_urls = ["http://kompas.com/"]
 
     rules = (
         # Extract links matching 'read' and parse them with the spider's method parse_item
@@ -81,13 +62,8 @@ class KompasSpider(Spider):
     if use Spider, change function to 'parse'
     if use CrawlSpider, change function to 'parse_item'
     """
-<<<<<<< HEAD
-    def parse_item(self, response):
-        log.msg("Get: %s" % response.url, level=log.DEBUG)
-=======
     def parse(self, response):
-        self.log('Hi, this is an item page! %s' % response.url)
->>>>>>> spider_kompas
+        log.msg("Get: %s" % response.url, level=log.DEBUG)
         news = NewsItem()
         news['url'] = response.url
         """Getting Timestamp and Provider"""
@@ -97,7 +73,6 @@ class KompasSpider(Spider):
         if "/login/" in response.url or "utm_source=wp" in response.url or "/sso/" in response.url:
             raise DropItem("URL not allowed")
 
-<<<<<<< HEAD
         if "nasional.kompas.com" in response.url:
             yield self.parse_item_default(response, news)
         elif "regional.kompas.com" in response.url:
@@ -461,114 +436,4 @@ class KompasSpider(Spider):
             return helper.formatted_date(u[5], u[6], u[7], "00", "00") - timedelta(hours=-7)
 
         return helper.formatted_date(u[4], u[5], u[6], "00", "00") - timedelta(hours=-7)
-=======
 
-        """Getting Title"""
-        if 'bola.kompas.com' in response.url:
-            title = response.xpath('//html/body/div/div[6]/div/div[1]/div[1]/div[2]/h2/text()').extract()
-        elif 'tekno.kompas.com' in response.url:
-            title = response.xpath('//html/body/div[1]/div[2]/div[5]/div[1]/div[2]/div/div[2]/div[1]/h1').extract()
-        elif 'entertainment.kompas.com' in response.url:
-            title = response.xpath('//html/body/div[2]/div/div/div[7]/div[4]/span[2]/text()').extract()
-        elif 'otomotif.kompas.com' in response.url:
-            title = response.xpath('//*[@id="skrol"]/div[1]/div[4]/h1/text()').extract()
-        elif 'health.kompas.com' in response.url:
-            title = response.xpath('//html/body/div[3]/div[4]/div[4]/h1/text()').extract()
-        elif 'female.kompas.com' in response.url:
-            title = response.xpath('//*[@id="wrap-skin"]/div/div[2]/div/div[1]/div[8]/div[3]/div[2]').extract()
-        elif 'properti.kompas.com' in response.url:
-            title = response.xpath('//html/body/div[1]/div[5]/div[3]/div[1]/div[2]/text()').extract()
-        else:
-            title = response.xpath('//h2/text()').extract()
-
-        news['title'] = title[0]
-
-
-        """Getting Content"""
-        if 'tekno.kompas.com' in response.url:
-            content = response.xpath('//html/body/div[1]/div[2]/div[5]/div[1]/div[2]/div/div[2]/div[2]/div[4]/text()').extract()
-        elif 'entertainment.kompas.com' in response.url:
-            content = response.xpath('//html/body/div[2]/div/div/div[7]/div[4]/div[9]/div[5]/text()').extract()
-        elif 'otomotif.kompas.com' in response.url:
-            content = response.xpath('//*[@id="skrol"]/div[1]/div[4]/div[4]/div[1]/p/text()').extract()
-        elif 'health.kompas.com' in response.url:
-            content = response.xpath('//html/body/div[3]/div[4]/div[4]/div[6]/div[5]/text()').extract()
-        elif 'female.kompas.com' in response.url:
-            content = response.css('#article_body').extract()[0]
-        elif 'properti.kompas.response' in response.url:
-            content = response.xpath('//html/body/div[1]/div[5]/div[3]/div[1]/div[8]/div[5]/text()').extract()
-        elif 'travel.kompas.com' in response.url:
-            content = response.css('.nml').extract()
-        else:
-            content = response.xpath('//span[@class="kcmread1114"]/p/text()').extract()
-
-        news['content'] = content[0]
-
-        """Getting Publish date"""""
-        if 'tekno.kompas.com' in response.url:
-            publish = response.xpath('html/body/div[1]/div[2]/div[5]/div[1]/div[2]/div/div[2]/div[2]/div[1]/div[1]/a[2]/text()').extract()
-        elif 'entertainment.kompas.com' in response.url:
-            publish = response.xpath('//html/body/div[2]/div/div/div[7]/div[4]/div[5]/div[1]/span[2]/text()').extract()
-        elif 'otomotif.kompas.com' in response.url:
-            publish = response.xpath('//*[@id="skrol"]/div[1]/div[4]/div[4]/div[1]/p/text()').extract()
-        elif 'health.kompas.com' in response.url:
-            publish = response.xpath('//html/body/div[3]/div[4]/div[4]/div[2]/div[1]/span/text()').extract()
-        elif 'female.kompas.com' in response.url:
-            publish = response.xpath('//*[@id="wrap-skin"]/div/div[2]/div/div[1]/div[8]/div[3]/div[4]/div[1]/span/text()').extract()
-        elif 'properti.kompas.response' in response.url:
-            publish = response.xpath('//html/body/div[1]/div[5]/div[3]/div[1]/div[4]/div[1]/span[2]/text()').extract()
-        else:
-            publish = response.xpath('//div[@class="grey small mb2"]/text()').extract()
-
-        news['publish'] = helper.kompas_date(publish[0]) - timedelta(hours=7)
-
-        """Getting Author"""
-        if 'tekno.kompas.com' in response.url:
-            author = response.xpath('//html/body/div[1]/div[2]/div[5]/div[1]/div[2]/div/div[2]/div[2]/div[1]/div[1]/a[1]/text()').extract()[0]
-        elif 'entertainment.kompas.com' in response.url:
-            author = response.xpath('//html/body/div[2]/div/div/div[7]/div[4]/div[11]/div[3]/text()').extract()[0]
-        elif 'otomotif.kompas.com' in response.url:
-            author = response.xpath('//*[@id="skrol"]/div[1]/div[4]/div[4]/div[2]/table/tbody/tr[1]/td[2]/text()').extract()[0]
-        elif 'health.kompas.com' in response.url:
-            author = response.xpath('//html/body/div[3]/div[4]/div[4]/div[2]/div[1]/span[1]/text()').extract()[0]
-        elif 'female.kompas.com' in response.url:
-            author = response.xpath('//*[@id="article_body"]/div[5]/text()').extract()[0]
-        elif 'properti.kompas.response' in response.url:
-            author = response.xpath('//html/body/div[1]/div[5]/div[3]/div[1]/div[4]/div[1]/span[1]/text()').extract()[0]
-        elif 'travel.kompas.com' in response.url:
-            author = response.xpath('//html/body/div[2]/div[5]/div/div[1]/div[2]/div[1]/h2/text()').extract()[0]
-        elif 'internasional.kompas.com' in response.url:
-            author = response.xpath('//html/body/div[1]/div[6]/div/div[1]/div[4]/div[3]/div[2]/div[2]/table/tbody/tr[1]/td[2]/text()').extract()[0]
-        elif 'bisniskeuangan.kompas.com' in response.url:
-            author = response.xpath('//html/body/div[2]/div[5]/div/div[1]/div[1]/div[3]/div[2]/div[2]/table/tbody/tr[1]/td[2]/text()').extract()[0]
-        elif 'bola.kompas.com' in response.url:
-            author = response.xpath('//html/body/div/div[6]/div/div[1]/div[1]/div[4]/div[2]/div[2]/table/tbody/tr[1]/td[2]').extract()[0]
-        else:
-            author = response.xpath('//html/body/div[1]/div[6]/div/div[1]/div[3]/div[3]/div[2]/div[2]/table/tbody/tr[1]/td[2]/text()').extract()[0]
-
-        news['author'] = author
-
-        """Getting location of news"""
-        if 'tekno.kompas.com' in response.url:
-            location = response.xpath('//html/body/div[1]/div[2]/div[5]/div[1]/div[2]/div/div[2]/div[2]/div[4]/strong/text()').extract()[0]
-        elif 'entertainment.kompas.com' in response.url:
-            location = response.xpath('//html/body/div[2]/div/div/div[7]/div[4]/div[9]/div[5]/strong/text()').extract()[0]
-        elif 'otomotif.kompas.com' in response.url:
-            location = response.xpath('//*[@id="skrol"]/div[1]/div[4]/div[4]/div[1]/p/strong[1]/text()').extract()[0]
-        elif 'health.kompas.com' in response.url:
-            location = ""
-        elif 'female.kompas.com' in response.url:
-            location = ""
-        elif 'properti.kompas.response' in response.url:
-            location = response.xpath('//html/body/div[1]/div[5]/div[3]/div[1]/div[8]/div[5]/strong/text()').extract()[0]
-        else:
-            location = news['content'][0:news['content'].find(',')]
-
-        news['location'] = location
-
-
-        """Getting Timestamp and Provider"""
-        news['timestamp']= datetime.utcnow()
-        news['provider'] = "kompas.com"
-        yield news
->>>>>>> spider_kompas
--- crawler/spiders/helper.py
+++ crawler/spiders/helper.py
@@ -10,7 +10,6 @@ from datetime import datetime
 def html_to_string(string_data):
     string_data = clear_script(string_data)
     data = lxml.html.fromstring(string_data)
-<<<<<<< HEAD
     data = lxml.html.tostring(data, method="text", encoding=unicode)
     return re.sub(' +',' ',data)
     
@@ -18,51 +17,6 @@ def clear_script(string_data):
     cleaner = Cleaner()
     cleaner.javascript = True
     cleaner.style = True
-=======
-    return lxml.html.tostring(data, method="text", encoding=unicode)
-
-
-def kompas_date(plain_string):
-    if plain_string == None:
-        return None
-
-    string_date = plain_string[plain_string.find(",") + 2 : plain_string.find("|") - 1]
-    string_time = plain_string[plain_string.find("|") + 2 : len(plain_string)]
-
-    date = string_date.split(" ")
-    day = date[0]
-    year = date[2]
-
-    ms = date[1]
-    month = ""
-    if ms == "Januari":
-        month = "01"
-    elif ms == "Februari":
-        month = "02"
-    elif ms == "Maret":
-        month = "03"
-    elif ms == "April":
-        month = "04"
-    elif ms == "Mei":
-        month = "05"
-    elif ms == "Juni":
-        month = "06"
-    elif ms == "Juli":
-        month = "07"
-    elif ms == "Agustus":
-        month = "08"
-    elif ms == "September":
-        month = "09"
-    elif ms == "Oktober":
-        month = "10"
-    elif ms == "November":
-        month = "11"
-    elif ms == "Desember":
-        month = "12"
-
-    hour = string_time[0:2]
-    minute = string_time[3:5]
->>>>>>> spider_kompas
 
     data = lxml.html.fromstring(string_data)
     return lxml.html.tostring(cleaner.clean_html(data))
