6b29d42a922b080befa17285fb4cbd9797acee29
zakazai <zaka.zaidan@ui.ac.id>
2015-04-12 20:00:06 +0700

    update beritasatu spider #6

--- crawler/spiders/BeritasatuSpider.py
+++ crawler/spiders/BeritasatuSpider.py
@@ -25,13 +25,18 @@ class BeritasatuSpider(CrawlSpider):
     allowed_domains = [
         "beritasatu.com",
     ]
-    start_urls = ["http://www.beritasatu.com/asia/260446-pemimpin-junta-thailand-ancam-eksekusi-jurnalis.html"]
+    start_urls = ["http://www.beritasatu.com/"]
 
     rules = (
         Rule(SgmlLinkExtractor(
-            allow=('indeks'),
-            deny=('reg', 'sso', 'login', 'utm_source=wp')),
+            allow=('nasional','dunia','megapolitan','ekonomi','properti','pasar-modal','sepakbola','olahraga','iptek','kesra','gaya-hidup','food-travel','hiburan','figur','pemilu-2014'),
+            deny=('reg', 'sso', 'login', 'utm_source=wp','member','video','galeri-foto','blog')),
+            follow=True),
+        Rule(SgmlLinkExtractor(
+            allow=('\w{2}[a-zA-Z0-9](/)\d[0-9]{5}'),
+            deny=('reg', 'sso', 'login', 'utm_source=wp','member','video','galeri-foto','blog'  )),
             follow=True,callback='parse_item'),
+
     )
 
     """
@@ -43,15 +48,21 @@ class BeritasatuSpider(CrawlSpider):
         news = NewsItem()
         news['url'] = response.url
 
-        if 'login' in response.url or 'utm_source=wp' in response.url or 'sso' in response.url or 'reg' in response.url:
+        if 'login' in response.url or 'utm_source=wp' in response.url or 'sso' in response.url or 'reg' in response.url or 'member' in response.url or 'video' in response.url or 'galeri-foto' in response.url:
                 raise DropItem("URL not allowed")
 
         title = response.xpath("//*/h1[contains(@class, 'mtb10') and contains(@class, 'fwnormal') and contains(@class, 'f30')]/text()").extract()[0]
-        publish = response.xpath("//*[contains(@class, 'c6') and contains(@class, 'left') and contains(@class, 'mt5')]/text()").extract()[0]
-        publish = self.berita_date(publish)
+        publish = response.xpath("//*[contains(@class, 'c6') and contains(@class, 'left') and contains(@class, 'mt5')]/text()").extract()
+        if len(publish) < 1:
+            publish = response.xpath("//*[contains(@class, 'c99') and contains(@class, 'left') and contains(@class, 'mt5')]/text()").extract()
+        publish = self.berita_date(publish[0])
         body = response.xpath("//*[contains(@class,'f14') and contains(@class,'c6') and contains(@class,bodyp)]").extract()
         body = helper.item_merge(body)
-        author = response.xpath("//*[text()[contains(.,'Penulis')]]/text()").extract()[0].split("Penulis: ")[1]
+        author = response.xpath("//*[text()[contains(.,'Penulis')]]/text()").extract()
+        if len(author) < 1:
+            author = None
+        elif isinstance(author,list):
+            author = author[0].split("Penulis: ")
         body = helper.clear_item(body)
         body = helper.html_to_string(body)
         location = body.split("-",1)[0].strip()
