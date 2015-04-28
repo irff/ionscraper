ac4dc6e08bdc2f5bd214e697622bd9be905ef977
kandito <kandito.agung@ui.ac.id>
2015-04-28 23:57:14 +0700

    perbaikan log scheduller

--- scheduller/job.py
+++ scheduller/job.py
@@ -1,6 +1,7 @@
 import sys
 import requests
 import logging
+from datetime import datetime
 from time import sleep
 
 logging.basicConfig(filename='job.log',level=logging.INFO)
@@ -11,19 +12,21 @@ PASS = "m4nt4bg4n"
 STOP_ALL = URL + "/index.html?action=stopall"
 
 if len(sys.argv) < 3:
+    logging.info(date_log + "Error command")
     exit()
 
 if sys.argv[1] not in ["start","stop","restart"]:
+    logging.info(date_log + "Error command")
     exit()
 
 action = sys.argv[1]
 
 requests.get(STOP_ALL, auth=(USER, PASS))
-sleep(10)
+sleep(30)
 proccessname = sys.argv[2]
 command = URL + "/index.html?processname=" + proccessname + "&action=" + action
 
 requests.get(command, auth=(USER, PASS))
-
-logging.info("starting " + proccessname + " command: " + command)
+date_log = "[" + str(datetime.now()) + "]"
+logging.info(date_log + "starting " + proccessname + " command: " + command)
 exit()
